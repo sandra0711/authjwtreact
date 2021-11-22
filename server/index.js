93057947f5382af6da0140a2d68e267ec15d46d2
Aleksandra Troshina <aatroshina@gmail.com>
2021-11-22 13:32:44 +0300

    start server&bd

---
+++ server/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const start = async () => {
+  const DB_URL = process.env.DB_URL
+  const PORT = process.env.PORT || 5000
+  try {
+    await mongoose.connect(DB_URL, { useNewUrlParser: true }, () => console.log('Подключились к базе данных'))
+    app.listen(PORT, () => console.log(`Сервер запущен по адресу ${PORT}`))
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+
+app.use(cors())
+app.use(cookieParser())
+
+start()
