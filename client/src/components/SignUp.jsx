3deb03ab1f77953da02307c3e37f8f8ebdb60afa
Aleksandra Troshina <aatroshina@gmail.com>
2021-12-08 17:56:20 +0300

    server&clientauthstart

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import Form from './Form';
+import { addNewUser } from '../store/slicers/userSlice';
 
 const Signup = () => {
   const dispatch = useDispatch();
   const handlerSignup = (email, password) => {
-    console.log(email, password)
+    console.log(email, password);
+    dispatch(addNewUser(email, password));
   };
   return (
     <div>
--- client/src/store/slicers/userSlice.js
+++ client/src/store/slicers/userSlice.js
@@ -1,4 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const addNewUser = createAsyncThunk(
+  'userSlice/addNewTodo',
+  async function ({ email, password }, { rejectWithValue, dispatch }) {
+    try {
+      const user = {
+        email,
+        password,
+      };
+      const response = await fetch(`http://localhost:5000/user/register`, {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+      if (!response.ok) throw new Error('Ошибка опять');
+      const data = await response.json();
+      dispatch(setUser(data));
+    } catch (e) {
+      return rejectWithValue(e.message)
+    }
+  },
+);
+
 
 const userSlice = createSlice({
   name: 'userSlice',
